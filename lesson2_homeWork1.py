823823d861e636cb96a4d472483f77d2291ba0d1
Nikita <dp140392@gmail.com>
2025-10-06 20:05:53 +0300

    second lesson2_homeWork1.py

---
+++ lesson2_homeWork1.py
@@ -0,0 +1,11 @@
+number = int(input("Введите четырехзначное число: "))
+
+number1 = number // 1000
+number2 = (number // 100) % 10
+number3 = (number // 10) % 10
+number4 = number % 10
+
+print(number1)
+print(number2)
+print(number3)
+print(number4)
