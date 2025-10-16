4179646a39099dc26b96a78a40968e9dae156c26
Nikita <dp140392@gmail.com>
2025-10-16 21:18:36 +0300

    lesson5_homeWork1.py

---
+++ lesson5_homeWork1.py
@@ -0,0 +1,30 @@
+import keyword
+import string
+
+name = input("введите строку: ")
+
+if name in keyword.kwlist:
+    print(False)
+    exit()
+if name[0].isdigit():
+    print(False)
+    exit()
+if any(ch.isupper() for ch in name):
+    print(False)
+    exit()
+for ch in name:
+    if ch in string.punctuation.replace("_", "") or ch == " ":
+        print(False)
+        exit()
+if name.count("_") > 1:
+    print(False)
+    exit()
+
+print(True)
+
+
+
+
+
+
+
