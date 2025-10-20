de0bfef61a8154af1e78f441c8c8283825807938
Nikita <dp140392@gmail.com>
2025-10-20 21:26:21 +0300

    lesson5_homeWork1.py

--- lesson5_homeWork1.py
+++ lesson5_homeWork1.py
@@ -1,20 +1,27 @@
 import keyword
 import string
+
 name = input("введите строку: ")
+if name == "_":
+    print(True)
+    exit()
+if "__" in name:
+    print(False)
+    exit()
 if name in keyword.kwlist:
     print(False)
     exit()
 if name[0].isdigit():
     print(False)
     exit()
+if any(ch in string.punctuation.replace("_", "") or ch == " " for ch in name):
+    print(False)
+    exit()
 if any(ch.isupper() for ch in name):
     print(False)
     exit()
-for ch in name:
-    if ch in string.punctuation.replace("_", "") or ch == " ":
-        print(False)
-        exit()
 print(True)
+
 
 
 
